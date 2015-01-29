cd5c2df7aee366b671301cc493641be8839968d6
Dpinwell <dompin@hotmail.co.uk>
2015-01-29 18:18:36 +0000

    lastest version

--- js/custom.js
+++ js/custom.js
@@ -15,7 +15,12 @@ $( document ).ready(function() {
 	    	var win = window.open("http://maps.google.com/maps?saddr=52.071637, -2.696374&daddr='"+userLocation, '_blank');
 	  		win.focus();
   		}
-  	});
+  	})
 
+    $(".menuOption1").click(function(){   
+    	$(".menuItems").fadeIn().animate({'margin-top':'4%'});
+    	$(".menuItems1").fadeIn().animate({'margin-top':'4%'});
+    	$(".menuItems2").fadeIn().animate({'margin-top':'4%'});
+    });
     
 });
